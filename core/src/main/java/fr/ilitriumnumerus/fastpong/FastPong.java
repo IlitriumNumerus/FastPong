f465a47ae06a5523e091445f291126195f3de81a
Benco11-developpement <65251723+benco11-developement@users.noreply.github.com>
2024-10-31 17:07:29 +0100

    Setup libgdx project

---
+++ core/src/main/java/fr/ilitriumnumerus/fastpong/FastPong.java
@@ -0,0 +1,53 @@
+package fr.ilitriumnumerus.fastpong;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class FastPong extends Game {
+    private SpriteBatch spriteBatch;
+    private Viewport viewport;
+    private ShapeRenderer shapeRenderer;
+
+    @Override
+    public void create() {
+        spriteBatch = new SpriteBatch();
+        shapeRenderer = new ShapeRenderer();
+        viewport = new FitViewport(80, 60);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void render() {
+        input();
+        logic();
+        draw();
+    }
+
+    private void input() {
+
+    }
+
+    private void logic() {
+
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        shapeRenderer.setProjectionMatrix(viewport.getCamera().combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+        shapeRenderer.rect(1, 1, 1, 1);
+        shapeRenderer.end();
+    }
+}
